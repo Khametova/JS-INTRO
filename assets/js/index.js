4a4fd58a2f92978e373e8bf52bb0840bf71260e6
Khametova <bilaneva19@gmail.com>
2025-06-03 21:19:23 +0200

    add .

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+alert("Hello teacher");
+
+console.log("Hi))");
+
+let userName = "Eva";
+console.log(userName);
+
+let age = "18";
+console.log("age", age);
+
+const constFunction = 11;
+console.log(constFunction);
+
+console.log('"java script"');
+
+const a = 5;
+const b = 2;
+console.log(a * b);
+
+const c = 12;
+const d = 6;
+console.log(c / d);
+
+const e = 3;
+const f = 4;
+console.log(e + f);
